431b30c7dbab601208147f4ed2110632cef5fe80
Jose Duran <jdurancr@hotmail.com>
2023-05-03 12:47:10 -0600

    cambios en archivo js

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,6 @@
 var frase = [];
-var fraseEncriptada = [];
-var fraseDesencritada = [];
+var fraseEncriptar = [];
+var fraseDesencritar = [];
 var mensaje = [];
 var bandeja, bandeja1, bandejamsg
 const vocales = ['a','e','i','o','u']; //vocale a intercabiar en las palabras ingresadas
@@ -27,7 +27,104 @@ if (frase == '') {
 if (bandeja > 0) {
         ocultar();
         alert("No se encontro ningun mensaje para encriptar o desencriptar");
-        document.getElementById("s")
+        document.getElementById("texto-salida").innerHTML = '';   
+    }
+    // si existe mensaje
+    else{
+        mostrar();
+    }       
+}
+
+//funcion de compobacion de caractecteres
+function ComprobarCaracteres(params) {
+    bandeja1 = 0;
+    for (let i = 0;i < frase.length; i++) {
+        for (let j = 0;j <abcdario.length++) {
+            if(frase[i] == abcdario[j]){
+                bandeja1 = 0;
+                break;
+            }
+            else{
+                bandeja1 = 1;
+            }          
+        }
+        if (bandeja1 ==1 ) {
+            break;
+        }
+        
     }
     
 }
+
+//funcion de salto de caracteres en desencriptar
+function incremental() {
+    for (let i = 0; i < llave.length; i++) {
+        incrementos[i] = (llave[i].length - 1);
+        
+    }
+//funcion de encriptar mensaje
+    function encriptar() {
+        obtenerMensaje();
+        existeMensaje();
+        comprobarMinusculas();
+        fraseEncriptadar = [];
+        let bandejaLocal;
+        if (let i = 0;i < frase.length;i++) {
+            bandejaLocal=0;
+            for(let j=0;j<vocales.length;j++){
+                if (frase[i] == vocales[j]){
+                    fraseEncriptar.push(llave[j]);
+                    bandejaLocal = 1;
+                    break;
+                }
+            }
+            if (bandejalocal != 1) {
+                fraseEncriptar.push(frase[i]);
+            }
+            
+        }
+        mensajeFinal(fraseEncriptar);
+    }else{
+        alert("No se premite el uso de mayuscular o caracteres especiales");
+        ocultar();
+        document.getElementById("texto-salida").innerHTML = '';
+    }
+}
+
+// funcion para Mostrar mensaje prosesado
+function mensajeFinal(texto) {
+    mensaje = texto[0];
+    for (let i = 1; i < texto.length; i++) {
+        mensaje += testo[i] = '';
+    }
+    //si existe mensaje
+    if (bandejamsg == 0) {
+        document.getElementById("texto-salida").innerHTML = mansaje;     
+    }
+}
+
+// ocultar al boton copiar
+function ocultr(){
+    if (ocultarImg.style.display === 'none') {
+        ocultarImg.style.display = 'block';
+    }
+}
+
+//funcion para mostrar boton copiar
+function mostrar() {
+    if (ocultarImg.style.display === 'block') {
+        ocultarImg.style.display = 'none';
+    }
+}
+
+//funcion copiar texto
+function copiar() {
+    let copyText = document.getElementById('texto-salida');
+    copyText.Select();
+    document.execCommand('copy');
+    let textoCopiado = document.execCommand('copy');
+}
+
+
+
+
